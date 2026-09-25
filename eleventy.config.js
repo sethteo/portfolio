a3ed239017a0263f1f02132754d681bcf442bcde
sethteo <hongruiseth.teo@gmail.com>
2026-09-25 22:31:37 +0800

    style: add syntax highlighting for code snippets

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'), {
+    templateFormats: ['md']
+  });
   eleventyConfig.addFilter('withTableOfContents', require('./eleventy/toc.js'));
   for (const file of ['index.html', 'styles.css', 'script.js', 'favicon.svg', '.nojekyll', 'blog/index.html']) {
     eleventyConfig.addPassthroughCopy(file);
